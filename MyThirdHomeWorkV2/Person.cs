48d10ffea2993a221e02018addf71a196b44442c
KrisjanisAboltins <krisjanis.aboltins@inbox.lv>
2022-04-24 21:16:08 +0300

    add

---
+++ MyThirdHomeWorkV2/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThirdHomeWorkV2
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Hobby { get; set; }
+
+        public string Gender { get; set; }
+
+        public int GetAge ()
+
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+
+        public void PersonGreeting() 
+        {
+            Console.WriteLine($"Hi, my name is {FirstName} and my last name is {LastName}, I am {Gender} and like {Hobby}");
+        }
+
+    }
+}
--- MyThirdHomeWorkV2/Program.cs
+++ MyThirdHomeWorkV2/Program.cs
@@ -4,103 +4,134 @@ using MyThirdHomeWorkV2;
 // Telefons - 1.Uzdevums
 MyThirdHomeWorkV2.MyPhoneHomeWorkV2 phone = new MyPhoneHomeWorkV2();
 
-//Console.WriteLine("Please write your phone lenght");
-//string lengthText = Console.ReadLine();
-//double length = double.Parse(lengthText);
-//phone.Length = length;
+Console.WriteLine("Please write your phone lenght");
+string lengthText = Console.ReadLine();
+double length = double.Parse(lengthText);
+phone.Length = length;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write your phone width");
-//string widthText = Console.ReadLine();
-//double width = double.Parse(widthText);
-//phone.Width = width;
+Console.WriteLine("Please write your phone width");
+string widthText = Console.ReadLine();
+double width = double.Parse(widthText);
+phone.Width = width;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//double phoneArea = length * width;
-//double phoneAreaRounded = Math.Round(phoneArea, 2);
-//phone.Size();
-//Console.WriteLine($"Your phone area is {phoneAreaRounded}");
+double phoneArea = length * width;
+double phoneAreaRounded = Math.Round(phoneArea, 2);
+phone.Size();
+Console.WriteLine($"Your phone area is {phoneAreaRounded}");
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write your phone brand");
-//string brandText = Console.ReadLine();
-//phone.Brand = brandText;
+Console.WriteLine("Please write your phone brand");
+string brandText = Console.ReadLine();
+phone.Brand = brandText;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write your phone model");
-//string modelText = Console.ReadLine();
-//phone.Model = modelText;
+Console.WriteLine("Please write your phone model");
+string modelText = Console.ReadLine();
+phone.Model = modelText;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write phone number to call");
-//string numberToCallText = Console.ReadLine();
-//phone.NumberToCall = numberToCallText;
+Console.WriteLine("Please write phone number to call");
+string numberToCallText = Console.ReadLine();
+phone.NumberToCall = numberToCallText;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write the SMS text");
-//string smsText = Console.ReadLine();
-//phone.SmsText = smsText;
+Console.WriteLine("Please write the SMS text");
+string smsText = Console.ReadLine();
+phone.SmsText = smsText;
 
-//Console.WriteLine();
-//Console.WriteLine("-------------------------------------------");
-//Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine();
 
-//phone.Greeting();
-//phone.Call();
-//phone.SMS();
+phone.Greeting();
+phone.Call();
+phone.SMS();
 
-//Console.WriteLine();
-//Console.WriteLine("===========================================");
-//Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("===========================================");
+Console.WriteLine();
 
 
 
 //// Automašīna - 2. Uzdevums
 
-//MyThirdHomeWorkV2.CarHomeWork3 myCar = new CarHomeWork3();
+MyThirdHomeWorkV2.CarHomeWork3 myCar = new CarHomeWork3();
 
-//Console.WriteLine("Please write your car brand");
-//string carBrandText = Console.ReadLine();
-//myCar.CarBrand = carBrandText;
+Console.WriteLine("Please write your car brand");
+string carBrandText = Console.ReadLine();
+myCar.CarBrand = carBrandText;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write your the car licence plate");
-//string carLicencePlateText = Console.ReadLine();
-//myCar.CarLicencePlate = carLicencePlateText;
+Console.WriteLine("Please write your the car licence plate");
+string carLicencePlateText = Console.ReadLine();
+myCar.CarLicencePlate = carLicencePlateText;
 
-//Console.WriteLine();
+Console.WriteLine();
 
-//Console.WriteLine("Please write driving speed");
-//string carSpeedText = Console.ReadLine();
-//double carSpeedNumber = double.Parse(carSpeedText);
-//myCar.CarSpeed = carSpeedNumber;
+Console.WriteLine("Please write driving speed");
+string carSpeedText = Console.ReadLine();
+double carSpeedNumber = double.Parse(carSpeedText);
+myCar.CarSpeed = carSpeedNumber;
 
-//Console.WriteLine();
-//Console.WriteLine("-------------------------------------------");
-//Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine();
 
-//myCar.StartToDrive();
-//myCar.IncreaseTheSpeed();
-//myCar.DecreaseTheSpeed();
+myCar.StartToDrive();
+myCar.IncreaseTheSpeed();
+myCar.DecreaseTheSpeed();
 
-//Console.WriteLine();
-//Console.WriteLine("===========================================");
-//Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("===========================================");
+Console.WriteLine();
 
 // Prece - 3. Uzdevums
 
-//MyThirdHomeWorkV2.ItemHomeWork3 myItem = new ItemHomeWork3(3, 3, 5);
+MyThirdHomeWorkV2.ItemHomeWork3 myItem = new ItemHomeWork3(3, 3, 5);
 
 
-//myItem.ItemParameters();
+myItem.ItemParameters();
+
+
+Console.WriteLine();
+Console.WriteLine("===========================================");
+Console.WriteLine();
+
 
 // Persona - 4. Uzdevums
 
+MyThirdHomeWorkV2.Person persona = new Person();
+
+Console.WriteLine("Please write your first name");
+string firstNameText = Console.ReadLine();
+persona.FirstName = firstNameText;
+
+Console.WriteLine("Please write your last name");
+string lastNameText = Console.ReadLine();
+persona.LastName = lastNameText;
+
+Console.WriteLine("Please write your BirthDate");
+string birthDatetext = Console.ReadLine();
+DateTime birthDateDate = DateTime.Parse(birthDatetext);
+persona.BirthDate = birthDateDate;
+
+Console.WriteLine("Please write your hobby");
+string hobbyText = Console.ReadLine();
+persona.Hobby = hobbyText;
+
+Console.WriteLine("Please write your gender");
+string genderText = Console.ReadLine();
+persona.Gender = genderText;
+
+persona.GetAge();
+persona.PersonGreeting();
 

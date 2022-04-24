422354317cfdb04ab764c4e1a37ce4785ddb1f78
KrisjanisAboltins <krisjanis.aboltins@inbox.lv>
2022-04-24 17:38:04 +0300

    Add project files.

---
+++ MyThirdHomeWorkV2/PhoneHomeWork3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThirdHomeWorkV2
+{
+    public class MyPhoneHomeWorkV2
+    {
+        
+        public double Length { get; set; }                      
+        public double Width { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string NumberToCall { get; set; }
+        public string SmsText{ get; set; }
+
+        public void Size()
+        {
+            Console.WriteLine($"Your phone lenght is {Length} and width is {Width}");
+        }
+
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Your new phone {Brand} , {Model}:");
+        }
+
+        public void Call()
+        {
+            Console.WriteLine($"    - is calling to the number {NumberToCall}");
+        }
+
+        public void SMS() 
+        {
+            Console.WriteLine($"    - and will send the SMS: {SmsText}, to the number {NumberToCall}");
+        }
+                  
+    }
+}
---
+++ MyThirdHomeWorkV2/Program.cs
@@ -0,0 +1,69 @@
+﻿
+using MyThirdHomeWorkV2;
+
+// Telefons - 1. Uzdevums
+MyThirdHomeWorkV2.MyPhoneHomeWorkV2 phone = new MyPhoneHomeWorkV2();
+
+Console.WriteLine("Please write your phone lenght");
+string lengthText = Console.ReadLine();
+double length = double.Parse(lengthText);
+phone.Length = length;
+
+Console.WriteLine();
+
+Console.WriteLine("Please write your phone width");
+string widthText = Console.ReadLine();
+double width = double.Parse(widthText);
+phone.Width = width;
+
+Console.WriteLine();
+
+double phoneArea = length * width;
+double phoneAreaRounded = Math.Round(phoneArea, 2);
+phone.Size();
+Console.WriteLine($"Your phone area is {phoneAreaRounded}");
+
+Console.WriteLine();
+
+Console.WriteLine("Please write your phone brand");
+string brandText = Console.ReadLine();
+phone.Brand = brandText;
+
+Console.WriteLine();
+
+Console.WriteLine("Please write your phone model");
+string modelText = Console.ReadLine();
+phone.Model = modelText;
+
+Console.WriteLine();
+
+Console.WriteLine("Please write phone number to call");
+string numberToCallText = Console.ReadLine();
+phone.NumberToCall = numberToCallText;
+
+Console.WriteLine();
+
+Console.WriteLine("Please write the SMS text");
+string smsText = Console.ReadLine();
+phone.SmsText = smsText;
+
+Console.WriteLine();
+Console.WriteLine("===========================================");
+Console.WriteLine();
+
+phone.Greeting();
+phone.Call();
+phone.SMS();
+
+Console.WriteLine();
+Console.WriteLine("===========================================");
+Console.WriteLine();
+
+
+
+// Automašīna - 2. Uzdevums
+
+
+
+
+
